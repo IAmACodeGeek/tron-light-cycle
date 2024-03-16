1b35ba002ab436dd84934977b8c1b4f0203cd595
IAmACodeGeek <copewithcode@gmail.com>
2024-03-16 19:34:33 +0530

    Bike controls setup and camera follow updated.

--- Assets/Project Assets/Scripts/Camera/CameraFollowPlayer.cs
+++ Assets/Project Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -1,14 +1,36 @@
+using System;
 using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour{
     public Transform target;
-    public Vector3 offset, lookOffset;
+    public Vector3 offset;
 
     public float smoothSpeed = 5f;
+    public float lookOffset = 1.75f;
 
-    private void LateUpdate(){
-        Vector3 desiredPosition = target.position + offset;
+    // For some high level math that i did but can never explain to anyone
+    private float FOLLOW_CIRCLE_RADIUS = 0f;
+    private float DELTA = 0f;
+
+    void Start(){
+        FOLLOW_CIRCLE_RADIUS = (float) Math.Sqrt(offset.x * offset.x + offset.y * offset.y);
+        DELTA = (float) Math.Tanh(offset.x / offset.z);
+        DELTA = Mathf.Deg2Rad * DELTA;
+    }
+
+    void LateUpdate(){
+        float theta = target.eulerAngles.y;
+        float omega = theta + DELTA;
+        omega = Mathf.Deg2Rad * omega;
+
+        float desiredX = (float)(target.position.x + Math.Sin(-omega) * FOLLOW_CIRCLE_RADIUS);
+        float desiredZ = (float)(target.position.z - Math.Cos(omega) * FOLLOW_CIRCLE_RADIUS);
+        float desiredY = target.position.y + offset.y;
+
+        Vector3 desiredPosition = new Vector3(desiredX, desiredY, desiredZ);
         transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.smoothDeltaTime);
-        transform.LookAt(target.position + lookOffset);
+        
+        Vector3 lookPosition = target.position + target.forward.normalized * lookOffset;
+        transform.LookAt(lookPosition);  
     }
 }
--- Assets/Project Assets/Scripts/Neon Bike/Movement.cs
+++ Assets/Project Assets/Scripts/Neon Bike/Movement.cs
@@ -2,25 +2,74 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour{
     private Rigidbody bikeBody;
-    public float forwardSpeed = 5f;
+
+    public float topSpeed = 40f;
+    private float currentSpeed = 0f;
+    public float accelerationFactor = 1f;
+    public float decelerationFactor = 1f;
+
+    // Degrees
+    private float maxLean = 30f;
+    private float currentLean = 0f;
+    public float leanFactor = 3f;
+    public float deleanFactor = 3f;
+    public float turnThreshold = 5f;
+    public float turnSpeedDependancyFactor = 0.15f;
+
+    public float maxTurnSpeed = 25f;
 
     private void Start(){
         bikeBody = GetComponent<Rigidbody>();
     }
 
     void Update(){
+        // Forward Movement
         if(Input.GetKey(KeyCode.UpArrow)){
-            Vector3 direction = transform.forward;
-            direction.Normalize();
-            bikeBody.velocity = direction * forwardSpeed;
-        }
-        else if(Input.GetKey(KeyCode.DownArrow)){
-            Vector3 direction = transform.forward * -1;
-            direction.Normalize();
-            bikeBody.velocity = direction * forwardSpeed;
+            Accelerate();
         }
         else{
-            bikeBody.velocity = Vector3.zero;
+            Decelerate();
         }
+
+        // Leaning left or right
+        if((Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow)) || !(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))){
+            DeLean();
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow)){
+            Lean(1);
+        }
+        else{
+            Lean(-1);
+        }
+
+        // Movement
+        Vector3 direction = transform.forward;
+        direction.Normalize();
+        Vector3 bikeVelocity = direction * currentSpeed;
+        transform.position = transform.position + bikeVelocity * Time.smoothDeltaTime;
+
+        // Lean and Change direction
+        float currentDirectionAngle = transform.eulerAngles.y;
+        currentDirectionAngle -= Time.smoothDeltaTime * maxTurnSpeed * (currentLean/maxLean);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, currentDirectionAngle, currentLean);
+    }
+
+    void Accelerate(){
+        currentSpeed = Mathf.Lerp(currentSpeed, topSpeed, accelerationFactor * Time.smoothDeltaTime);
+    }
+
+    void Decelerate(){
+        currentSpeed = Mathf.Lerp(currentSpeed, 0f, decelerationFactor * Time.smoothDeltaTime);
+    }
+
+    void Lean(int direction){
+        if(currentSpeed > turnThreshold)
+            currentLean = Mathf.Lerp(currentLean, direction * maxLean * (1f - (currentSpeed/topSpeed)*turnSpeedDependancyFactor), leanFactor * Time.smoothDeltaTime);
+        else
+            DeLean();
+    }
+
+    void DeLean(){
+        currentLean = Mathf.Lerp(currentLean, 0f, deleanFactor * Time.smoothDeltaTime);
     }
 }
