db207aee60610f158834f1c0e1c0e0dd29e922a1
IAmACodeGeek <copewithcode@gmail.com>
2024-03-17 16:08:56 +0530

    Tweaks in the camera follow to make smooth turn transitions

--- Assets/Project Assets/Scripts/Camera/CameraFollowPlayer.cs
+++ Assets/Project Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -8,12 +8,15 @@ public class CameraFollowPlayer : MonoBehaviour{
     public float smoothSpeed = 5f;
     public float lookOffset = 1.75f;
 
+    private float currentOmega = 0f;
+    public float angularSmoothSpeed = 30f;
+
     // For some high level math that i did but can never explain to anyone
     private float FOLLOW_CIRCLE_RADIUS = 0f;
     private float DELTA = 0f;
 
     void Start(){
-        FOLLOW_CIRCLE_RADIUS = (float) Math.Sqrt(offset.x * offset.x + offset.y * offset.y);
+        FOLLOW_CIRCLE_RADIUS = (float) Math.Sqrt(offset.x * offset.x + offset.z * offset.z);
         DELTA = (float) Math.Tanh(offset.x / offset.z);
         DELTA = Mathf.Deg2Rad * DELTA;
     }
@@ -21,10 +24,14 @@ public class CameraFollowPlayer : MonoBehaviour{
     void LateUpdate(){
         float theta = target.eulerAngles.y;
         float omega = theta + DELTA;
-        omega = Mathf.Deg2Rad * omega;
 
-        float desiredX = (float)(target.position.x + Math.Sin(-omega) * FOLLOW_CIRCLE_RADIUS);
-        float desiredZ = (float)(target.position.z - Math.Cos(omega) * FOLLOW_CIRCLE_RADIUS);
+        omega = Mathf.Deg2Rad * omega;
+        if(omega - currentOmega > 5) currentOmega += 2 * Mathf.PI;
+        else if(omega - currentOmega < -5) currentOmega -= 2 * Mathf.PI;
+        currentOmega = Mathf.Lerp(currentOmega, omega, angularSmoothSpeed * Time.smoothDeltaTime);
+
+        float desiredX = (float)(target.position.x + Math.Sin(-currentOmega) * FOLLOW_CIRCLE_RADIUS);
+        float desiredZ = (float)(target.position.z - Math.Cos(currentOmega) * FOLLOW_CIRCLE_RADIUS);
         float desiredY = target.position.y + offset.y;
 
         Vector3 desiredPosition = new Vector3(desiredX, desiredY, desiredZ);
